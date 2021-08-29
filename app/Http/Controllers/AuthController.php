1d6710b588702c48a4496871c0861c576e5825f5
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-08-29 12:30:21 -0300

    create auhcontroller method register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterUserRequest $request)
+    {
+        $payload = $request->all();
+        $payload['password'] = Hash::make($payload['password']);
+        
+        User::create($payload);
+
+        return $this->sucess([
+            'user' => $user,
+            'token' => $user->createToken('API token')->plainTextToken
+        ]);
+
+    }
 }
